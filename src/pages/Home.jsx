0d254517236e8329158d8a9931749e5975afc9cc
Rulif Fadria Nirwansyah <ruliffax@gmail.com>
2026-02-09 01:41:36 +0700

    Remove helmet and revert SEO setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async";
 import Hero from "../components/sections/Hero";
 import About from "../components/sections/About";
 import Features from "../components/sections/Features";
@@ -11,24 +10,6 @@ import Navbar from "../components/layout/Navbar";
 const Home = () => {
   return (
     <>
-      <Helmet>
-        <title>Furniture Store</title>
-        <meta name="description" content="Modern furniture for your home" />
-
-        {/* Open Graph */}
-        <meta property="og:title" content="Furniture Store" />
-        <meta property="og:description" content="Modern furniture for your home" />
-        <meta
-          property="og:image"
-          content="https://furniture-react-landing-page-rulif.vercel.app/preview.jpg"
-        />
-        <meta
-          property="og:url"
-          content="https://furniture-react-landing-page-rulif.vercel.app/"
-        />
-        <meta property="og:type" content="website" />
-      </Helmet>
-
       <Navbar />
 
       <main>
