e7bbd8ee91747fa2efef1c931f23f7c5ea588aa4
Rulif Fadria Nirwansyah <ruliffax@gmail.com>
2026-02-09 01:28:06 +0700

    Add react-helmet-async for SEO metadata

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Hero from "../components/sections/Hero";
 import About from "../components/sections/About";
 import Features from "../components/sections/Features";
@@ -10,6 +11,24 @@ import Navbar from "../components/layout/Navbar";
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Furniture Store</title>
+        <meta name="description" content="Modern furniture for your home" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="Furniture Store" />
+        <meta property="og:description" content="Modern furniture for your home" />
+        <meta
+          property="og:image"
+          content="https://furniture-react-landing-page-rulif.vercel.app/preview.jpg"
+        />
+        <meta
+          property="og:url"
+          content="https://furniture-react-landing-page-rulif.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
       <Navbar />
 
       <main>
